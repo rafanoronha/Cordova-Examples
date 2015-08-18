b3870c08474598aee3286d7a5882925dcc89da44
rafanoronha <rafael.noronha@lambda3.com.br>
2015-08-18 15:15:01 -0300

    Added hook

---
+++ hooks/after_platform_add/install_plugins.js
@@ -0,0 +1,16 @@
+var pluginlist = [
+    "org.apache.cordova.contacts",
+];
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
+
+pluginlist.forEach(function(plug) {
+    exec("cordova plugin add " + plug, puts);
+});
